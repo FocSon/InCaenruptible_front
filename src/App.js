56597cbdad2b5058fe32d4417009c2f73d777cb3
Nicolas Rousseau <nicolasrousseau@Nicolass-MacBook-Air.local>
2024-01-16 16:56:24 +0100

    NavBar done, project react init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './Dashboard'; // Adjust the path according to your project structure
+import logo from './logo.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  useEffect(() => {
+    // Set the data-theme attribute to "dark" on the html element
+    document.documentElement.setAttribute('data-theme', 'light');
+  }, []);
+    return (
+      
+      <header>
+      <Router>
+        <nav>
+        <img src={logo} alt="Logo" className="navbar-logo" />
 
+          <ul>
+            <li><Link to="/dashboard">Principal</Link></li>
+            <li><Link to="/tasks">Posts</Link></li>
+            <li><Link to="/penalty">Evenement</Link></li>
+         
+            {/* more links */}
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          
+          {/* more routes */}
+        </Routes>
+      </Router>
+    </header>
+  
+  );
+  
+}
 export default App;
+
