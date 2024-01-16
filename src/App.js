3e4720db435cef7fbc35b83483daa3c73d5b2d5d
FocSon <focson@yopmail.com>
2024-01-16 20:12:45 +0100

    refactor navbar et css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Dashboard from './Dashboard'; // Adjust the path according to your project structure
-import logo from './logo.png'
+import Navbar from "./component/Navbar";
 
 function App() {
   
@@ -10,29 +8,8 @@ function App() {
     document.documentElement.setAttribute('data-theme', 'light');
   }, []);
     return (
-    <header>
-      <Router>
-        <nav>
-          <img src={logo} alt="Logo" className="navbar-logo" />
+    <Navbar />
 
-          <ul>
-            <li><Link to="/dashboard">Principal</Link></li>
-            <li><Link to="/tasks">Posts</Link></li>
-            <li><Link to="/penalty">Evenement</Link></li>
-
-            {/* more links */}
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-
-
-          {/* more routes */}
-        </Routes>
-      </Router>
-    </header>
-  
   );
   
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import logo from "../logo.png";
+import Dashboard from "../Dashboard";
+
+function Navbar() {
+    return (
+        <Router>
+            <nav>
+                <img src={logo} alt="Logo" className="navbar-logo" />
+
+                <ul>
+                    <li><Link to="*">Principal</Link></li>
+                    <li><Link to="/posts">Posts</Link></li>
+                    <li><Link to="/events">Evenements</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default Navbar;
