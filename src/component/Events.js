c78c66e63e3fa9bf152d4af99670f9654255669b
florian <florian.blaise@ecole.ensicaen.fr>
2024-01-17 13:38:15 +0100

    test new directory

--- src/component/Events.js
+++ src/component/Events.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Event = () => {
     return (
         <div>
-            <h2>Welcome to the Event Page</h2>
+            <h2>Welcome to the Event Page, this is the new repo for me</h2>
         </div>
     );
 };
