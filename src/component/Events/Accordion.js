8e256c12a78beaca348143315e40c31f3e097569
florian <florian.blaise@ecole.ensicaen.fr>
2024-01-17 13:42:23 +0100

    fixed my repo, catching up on old changes. Added accordeon on the event page and added a first style

---
+++ src/component/Events/Accordion.js
@@ -0,0 +1,40 @@
+// src/components/Accordion.js
+import React, { useState } from 'react';
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Box } from '@chakra-ui/react';
+
+function AccordionComponent({ items }) {
+  const [accordionItems, setAccordionItems] = useState(items || []);
+
+  const handleToggle = (index) => {
+    setAccordionItems((prevItems) => {
+      const newItems = [...prevItems];
+      newItems[index].isOpen = !newItems[index].isOpen;
+      return newItems;
+    });
+  };
+
+  return (
+    <Box p={5}>
+      <h1>Fil des alertes</h1>
+      <Accordion allowToggle>
+        {accordionItems.map((item, index) => (
+          <AccordionItem key={index}>
+            <h2>
+              <AccordionButton onClick={() => handleToggle(index)} _expanded={{ bg: 'tomato', color: 'white' }}>
+                <Box flex="1" textAlign="left">
+                  {item.title}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <p>{item.content}</p>
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </Box>
+  );
+}
+
+export default AccordionComponent;
