592f3939400fb6fdeb1b7b11b925f6b210535bea
SoleFyhr <rousseaunico@free.fr>
2024-01-18 13:20:31 +0100

    Deleted Dashboard and glowed up the Form in the CreateAlert.js

--- src/component/CreateAlertForm.js
+++ src/component/CreateAlertForm.js
@@ -1,76 +1,225 @@
-import React, {useState} from 'react';
-import './CreateAlertForm.css';
+import React, { useState } from "react";
+import "./CreateAlertForm.css";
+import {
+  Modal,
+  FormControl,
+  useDisclosure,
+  ModalBody,
+  FormLabel,
+  Input,
+  ModalFooter,
+  Button,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  Select,
+  FormErrorMessage
+} from "@chakra-ui/react";
 
-function CreateAlertForm(properties) {
-    const {homeService} = properties;
+function Alert(properties) {
+    const { homeService } = properties;
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const [formData, setFormData] = useState({
-        type: 'video',
-        category: 'city',
-        title: '',
-        description: '',
-    });
+  const [formData, setFormData] = useState({
+    type: "video",
+    category: "city",
+    title: "",
+    description: "",
+  });
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        });
-    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
 
-        if(formData.title !== '' || formData.description !== '') {
-            homeService.createAlert(formData.type, formData.category, formData.title, formData.description).then(r => {})
-        }
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-        setFormData({
-            type: 'video',
-            category: 'city',
-            title: '',
-            description: '',
-        });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.title !== "" && formData.description !== "") {
+      homeService.createAlert(
+        formData.type,
+        formData.category,
+        formData.title,
+        formData.description
+      ).then((r) => {
+        // Handle response
+      });
+    }
+    onClose(); // Close the modal after submission
+  };
 
-    return (
-        <div className="Home">
-            <form onSubmit={handleSubmit}>
-                <div className="form-entry">
-                    <label htmlFor="type">Type</label>
-                    <select name="type" onChange={handleChange} value={formData.type} className="entry">
-                        <option value="video">Video</option>
-                        <option value="co2">CO2 level</option>
-                        <option value="sound">Sound level</option>
-                    </select>
-                </div>
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+  
 
-                <div className="form-entry">
-                    <label htmlFor="category">Type</label>
-                    <select name="category" onChange={handleChange} value={formData.category}
-                            className="entry">
-                        <option value="city">City</option>
-                        <option value="company">Company</option>
-                        <option value="other">Other</option>
-                    </select>
-                </div>
+  return (
+    <>
+      <Button colorScheme="messenger" onClick={onOpen}>
+        Créer une Alerte
+      </Button>
 
-                <div className="form-entry">
-                    <label htmlFor="title">Title</label>
-                    <input className="entry" onChange={handleChange} value={formData.title}
-                           name="title"></input>
-                </div>
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Alerte</ModalHeader>
+          <ModalCloseButton />
+          <form onSubmit={handleSubmit}>
+            <ModalBody pb={6}>
+              <FormControl isRequired>
+                <FormLabel>Type d'Alerte</FormLabel>
+                <Select name="type" onChange={handleInputChange} value={formData.type}>
+                  <option value="video">Live</option>
+                  <option value="co2">Taux de CO2</option>
+                  <option value="sound">Niveau Sonore</option>
+                </Select>
+                
+              </FormControl>
 
-                <div className="form-entry">
-                    <label for="description">Description</label>
-                    <textarea className="entry" onChange={handleChange} value={formData.description}
-                              name="description"></textarea>
-                </div>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>Sujet d'Alerte</FormLabel>
+                <Select name="category" onChange={handleInputChange} value={formData.category}>
+                  <option value="city">Ville</option>
+                  <option value="company">Entreprise</option>
+                  <option value="other">Autre</option>
+                </Select>
+              </FormControl>
 
-                <button className="form-entry" type="submit">Launch an alert</button>
-            </form>
-        </div>
-    );
+              <FormControl isRequired mt={4}>
+                <FormLabel>Titre</FormLabel>
+                <Input
+                  name="title"
+                  onChange={handleInputChange}
+                  value={formData.title}
+                  placeholder="Entrez un titre pour votre alerte"
+                />
+              </FormControl>
+
+              <FormControl isRequired mt={4}>
+                <FormLabel>Description</FormLabel>
+                <Input
+                  name="description"
+                  onChange={handleInputChange}
+                  value={formData.description}
+                  placeholder="Entrez une description pour votre alerte"
+                />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button type="submit" colorScheme="blue" mr={3}>
+                Lancer une alerte
+              </Button>
+              <Button onClick={onClose}>Annuler</Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
-export default CreateAlertForm;
+// function CreateAlertForm(properties) {
+//   const { homeService } = properties;
+
+//   const [formData, setFormData] = useState({
+//     type: "video",
+//     category: "city",
+//     title: "",
+//     description: "",
+//   });
+
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (formData.title !== "" || formData.description !== "") {
+//       homeService
+//         .createAlert(
+//           formData.type,
+//           formData.category,
+//           formData.title,
+//           formData.description
+//         )
+//         .then((r) => {});
+//     }
+
+//     setFormData({
+//       type: "video",
+//       category: "city",
+//       title: "",
+//       description: "",
+//     });
+//   };
+
+//   return (
+//     <div className="Home">
+//       <form onSubmit={handleSubmit}>
+//         <div className="form-entry">
+//           <label htmlFor="type">Type</label>
+//           <select
+//             name="type"
+//             onChange={handleChange}
+//             value={formData.type}
+//             className="entry"
+//           >
+//             <option value="video">Video</option>
+//             <option value="co2">CO2 level</option>
+//             <option value="sound">Sound level</option>
+//           </select>
+//         </div>
+
+//         <div className="form-entry">
+//           <label htmlFor="category">Type</label>
+//           <select
+//             name="category"
+//             onChange={handleChange}
+//             value={formData.category}
+//             className="entry"
+//           >
+//             <option value="city">City</option>
+//             <option value="company">Company</option>
+//             <option value="other">Other</option>
+//           </select>
+//         </div>
+
+//         <div className="form-entry">
+//           <label htmlFor="title">Title</label>
+//           <input
+//             className="entry"
+//             onChange={handleChange}
+//             value={formData.title}
+//             name="title"
+//           ></input>
+//         </div>
+
+//         <div className="form-entry">
+//           <label for="description">Description</label>
+//           <textarea
+//             className="entry"
+//             onChange={handleChange}
+//             value={formData.description}
+//             name="description"
+//           ></textarea>
+//         </div>
+
+//         <button className="form-entry" type="submit">
+//           Launch an alert
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+export default Alert;
